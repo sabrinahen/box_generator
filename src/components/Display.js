7e0689ce28bf14a2fe1800905e10851de2336008
Sabrina Henrice <sabrinahenrice@gmail.com>
2022-02-24 22:30:06 -0500

    final

---
+++ src/components/Display.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Display = (props) => {
+    const { boxArray } = props;
+
+    return (
+        <div>
+        {
+            boxArray.map((box, index) => (
+                <div key={index} style={{ 
+                    display: "inline-block",
+                    margin: "10px",
+                    height: box.size, 
+                    width: box.size, 
+                    backgroundColor: box.color
+                }}>
+                </div>
+            ))
+        }
+        </div>
+    );
+    }
+    
+export default Display;
